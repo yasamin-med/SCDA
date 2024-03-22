d9772400acfc0770f5ab9a6458a2b4cc05acef09
yasamin-med <88545679+yasamin-med@users.noreply.github.com>
2024-03-22 12:53:40 -0700

    Update make_dataset_breast.py

--- make_dataset_breast.py
+++ make_dataset_breast.py
@@ -4,58 +4,47 @@ import os
 from pathlib import Path
 import torch
 import cv2
+import argparse
 
+def list_of_strings(arg):
+    return arg.split(',')
+parser = argparse.ArgumentParser(description='Preparing Datasets',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('--train_dir', type=str, default="", help= "directory of train dataset")
+parser.add_argument('--classes', type= list_of_strings, default=['benign','malignant','normal'], help= "name of classes with camma and no space")
+parser.add_argument('--prompt_structure', type=str, default= "an ultrasound photo of {class_name} tumor in breast", help= "structure of prompt")
+parser.add_argument('--dataset_path', type=str, default= "yasimed/split_dataset", help= "path of dataset in hugging face")
+parser.add_argument('--token', type=str, default= "", help= "token of access")
+args = parser.parse_args()
+print(args)
 
 
 path_image = []
 text_image = []
-root_train_dataset = "/home/yasamin/Documents/Covid Dataset/Original/split0"
-root_main = root_train_dataset + "/covid"
+
 
 
 count = 0
-for root, dirs, files in os.walk(root_main):
-    for name in files:
-        path_image.append((os.path.join( root , name)))
-        prompt = "an ultrasound photo of covid lung"
-        
-        temp = Image((os.path.join( root , name)))
-        count = count + 1
-        
-        text_image.append(prompt)
+for class_name in args.classes:
+    root_main = os.path.join(args.train_dir , class_name)
+    
+    for root, dirs, files in os.walk(root_main):
+        for name in files:
+            path_image.append((os.path.join( root , name)))
+            prompt = args.prompt_structure.replace("{class_name}" , class_name)
+            
+            temp = Image((os.path.join( root , name)))
+            count = count + 1
+            
+            text_image.append(prompt)
 
-root_main = root_train_dataset + "/pneumonia"
-
-
-for root, dirs, files in os.walk(root_main):
-    for name in files:
-        path_image.append((os.path.join( root , name)))
-        prompt = "an ultrasound photo of pneumonia lung"
-        
-        temp = Image((os.path.join( root , name)))
-        count = count + 1
-        
-        text_image.append(prompt)
-
-root_main = root_train_dataset + "/regular"
-
-
-for root, dirs, files in os.walk(root_main):
-    for name in files:
-        path_image.append((os.path.join( root , name)))
-        prompt = "an ultrasound photo of regular lung"
-        
-        temp = Image((os.path.join( root , name)))
-        count = count + 1
-        
-        text_image.append(prompt)
-
-print(len(text_image))
-print(len(path_image))
+print("Number of Prompts: ", len(text_image))
+print("Number of Images: ", len(path_image))
 dataset = Dataset.from_dict({"image": path_image , "text": text_image}).cast_column("image", Image())
 
-dataset.push_to_hub("yasimed/Covid_dataset", private=True)
+dataset.push_to_hub(args.dataset_path, private=True)
 
 
-dataset = load_dataset("yasimed/Covid_dataset",use_auth_token="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")
+dataset = load_dataset(args.dataset_path,use_auth_token=args.token)
+
 print(dataset)
